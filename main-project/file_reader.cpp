ac6510061f527cced0be43c649ef8e17e4e6361b
nvorderryy <cobrazmeika3@gmail.com>
2024-12-17 21:33:06 +0300

    Реализовано чтение данных из файла

---
+++ main-project/file_reader.cpp
@@ -0,0 +1,46 @@
+#include "file_reader.h"
+#include "constants.h"
+
+#include <fstream>
+#include <cstring>
+
+time1 convert_to_time(char* str)
+{
+    time1 result;
+    char* context = NULL;
+    char* str_number = strtok_s(str, ":", &context);
+    result.hours = atoi(str_number);
+    str_number = strtok_s(NULL, ":", &context);
+    result.minutes = atoi(str_number);
+    str_number = strtok_s(NULL, ":", &context);
+    result.seconds = atoi(str_number);
+    return result;
+}
+
+void read(const char* file_name, internet_protocol* array[], int& size)
+{
+    std::ifstream file(file_name);
+    if (file.is_open())
+    {
+        size = 0;
+        char tmp_buffer[MAX_STRING_SIZE];
+        while (!file.eof() && size < MAX_FILE_ROWS_COUNT)
+        {
+            internet_protocol* item = new internet_protocol;
+            file >> tmp_buffer;
+            item->start = convert_to_time(tmp_buffer);
+            file >> tmp_buffer;
+            item->finish = convert_to_time(tmp_buffer);
+            file >> item->receive.kbytes;
+            file >> item->send.kbytes;
+            file.ignore(); // Пропуск пробела перед чтением пути
+            file.getline(item->path, MAX_STRING_SIZE);
+            array[size++] = item;
+        }
+        file.close();
+    }
+    else
+    {
+        throw "Ошибка открытия файла";
+    }
+}
--- main-project/main.cpp
+++ main-project/main.cpp
@@ -2,10 +2,45 @@
 
 using namespace std;
 
+#include "internet_protocol.h"
+#include "file_reader.h"
+#include "constants.h"
+
 int main()
 {
-    cout << "Laboratory work #8. GIT\n";
-    cout << "Variant #5. Internet protocol\n";
-    cout << "Author: Egor Kuznechik\n";
+    setlocale(LC_ALL, "Russian");
+    cout << "Лабораторная работа №8. GIT\n";
+    cout << "Вариант №5. Протокол Интернет\n";
+    cout << "Автор: Egor Kuznechik\n\n";
+
+    internet_protocol* protocols[MAX_FILE_ROWS_COUNT];
+    int size;
+
+    try
+    {
+        read("data.txt", protocols, size);
+        for (int i = 0; i < size; i++)
+        {
+            cout << protocols[i]->start.hours << ':';
+            cout << protocols[i]->start.minutes << ':';
+            cout << protocols[i]->start.seconds << '\n';
+            cout << protocols[i]->finish.hours << ':';
+            cout << protocols[i]->finish.minutes << ':';
+            cout << protocols[i]->finish.seconds << '\n';
+            cout << protocols[i]->receive.kbytes << '\n';
+            cout << protocols[i]->send.kbytes << '\n';
+            cout << protocols[i]->path << '\n';
+            cout << '\n';
+        }
+        for (int i = 0; i < size; i++)
+        {
+            delete protocols[i];
+        }
+    }
+    catch (const char* error)
+    {
+        cout << error << '\n';
+    }
+
     return 0;
 }
